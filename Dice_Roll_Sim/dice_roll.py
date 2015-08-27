f78f75c5713fba5a9295ba9000467b6da9d9aaa7
MattiasZurkovic <mattias@zurkovic.com>
2015-08-27 17:26:24 -0400

    add

--- Dice_Roll_Sim/dice_roll.py
+++ Dice_Roll_Sim/dice_roll.py
@@ -1,18 +1,38 @@
 import sys
 import random
+import time
 
 def __init__():
   roll()
 
-def roll():
-  # Generates random numbers between 1 and 6
-  ch = random.randint(1,6)
-  print(ch)
-  print(side_number())
+prev_rolls = []
 
-def side_number():
+def roll():
+  # ans = how many sides dice has (i.e. 20)
   ans = int(input('How many sides does your dice have? (i.e. 6): '))
-  return ans
+  # Generates random numbers between 1 and ans*
+  land = random.randint(1, ans)
+  
+  rolling_ani()
+  print(land)
+  re_roll()
+
+def re_roll():
+  # User response
+  res = input('Roll again? (Y/N): ')
+  if res == 'Y':
+    roll()
+
+def rolling_ani():
+  print('Rolling.')
+  time.sleep(0.5)
+  print('Rolling..')
+  time.sleep(0.5)
+  print('Rolling...')
+  time.sleep(0.5)
+  print('Rolling....')
+
+  print('You rolled:')
 
 __init__()
   
